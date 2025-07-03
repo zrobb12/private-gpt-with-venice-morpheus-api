3cf0995fa66622b9ad2a7da636020eac9dc74604
zrobb12 <96152882+zrobb12@users.noreply.github.com>
2025-07-02 20:54:44 -0400

    Update embedding_component.py

--- private_gpt/components/embedding/embedding_component.py
+++ private_gpt/components/embedding/embedding_component.py
@@ -1,10 +1,15 @@
 import logging
 
 from injector import inject, singleton
-from llama_index.core.embeddings import BaseEmbedding, MockEmbedding
+from llama_index.core.embeddings import BaseEmbedding
+from llama_index.core.settings import Settings as LlamaIndexSettings
 
-from private_gpt.paths import models_cache_path
+from private_gpt.paths import models_cache_path, models_path
 from private_gpt.settings.settings import Settings
+
+# Custom Embeddings
+from private_gpt.components.embedding.custom.venice import VeniceEmbedding
+from private_gpt.components.embedding.custom.morpheus import MorpheusEmbedding
 
 logger = logging.getLogger(__name__)
 
@@ -16,110 +21,77 @@ class EmbeddingComponent:
     @inject
     def __init__(self, settings: Settings) -> None:
         embedding_mode = settings.embedding.mode
-        logger.info("Initializing the embedding model in mode=%s", embedding_mode)
-        match embedding_mode:
-            case "huggingface":
+        logger.info("Initializing the Embedding model in mode=%s", embedding_mode)
+        match settings.embedding.mode:
+            case "llamacpp":
                 try:
-                    from llama_index.embeddings.huggingface import (  # type: ignore
-                        HuggingFaceEmbedding,
-                    )
+                    from llama_index.embeddings.llama_cpp import LlamaCPPEmbedding  # type: ignore
                 except ImportError as e:
                     raise ImportError(
-                        "Local dependencies not found, install with `poetry install --extras embeddings-huggingface`"
+                        "LlamaCPP embedding dependencies not found, install with `poetry install --extras llms-llama-cpp`"
                     ) from e
 
-                self.embedding_model = HuggingFaceEmbedding(
-                    model_name=settings.huggingface.embedding_hf_model_name,
-                    cache_folder=str(models_cache_path),
-                    trust_remote_code=settings.huggingface.trust_remote_code,
+                self.embedding_model = LlamaCPPEmbedding(
+                    model_path=str(models_path / settings.llamacpp.embedding_hf_model_file),
+                    cache_dir=str(models_cache_path),
+                    max_seq_length=settings.embedding.max_seq_length,
+                    context_window=settings.embedding.context_window,
                 )
-            case "sagemaker":
-                try:
-                    from private_gpt.components.embedding.custom.sagemaker import (
-                        SagemakerEmbedding,
-                    )
-                except ImportError as e:
-                    raise ImportError(
-                        "Sagemaker dependencies not found, install with `poetry install --extras embeddings-sagemaker`"
-                    ) from e
 
-                self.embedding_model = SagemakerEmbedding(
-                    endpoint_name=settings.sagemaker.embedding_endpoint_name,
-                )
             case "openai":
                 try:
-                    from llama_index.embeddings.openai import (  # type: ignore
-                        OpenAIEmbedding,
-                    )
+                    from llama_index.embeddings.openai import OpenAIEmbedding  # type: ignore
                 except ImportError as e:
                     raise ImportError(
-                        "OpenAI dependencies not found, install with `poetry install --extras embeddings-openai`"
+                        "OpenAI embedding dependencies not found, install with `poetry install --extras llms-openai`"
                     ) from e
 
-                api_base = (
-                    settings.openai.embedding_api_base or settings.openai.api_base
-                )
-                api_key = settings.openai.embedding_api_key or settings.openai.api_key
-                model = settings.openai.embedding_model
-
+                openai_settings = settings.openai
                 self.embedding_model = OpenAIEmbedding(
-                    api_base=api_base,
-                    api_key=api_key,
-                    model=model,
+                    api_base=openai_settings.api_base,
+                    api_key=openai_settings.api_key,
+                    model=openai_settings.embedding_model,
                 )
-            case "ollama":
+
+            case "openailike":
                 try:
-                    from llama_index.embeddings.ollama import (  # type: ignore
-                        OllamaEmbedding,
-                    )
-                    from ollama import Client  # type: ignore
+                    from llama_index.embeddings.openai_like import OpenAILikeEmbedding  # type: ignore
                 except ImportError as e:
                     raise ImportError(
-                        "Local dependencies not found, install with `poetry install --extras embeddings-ollama`"
+                        "OpenAILike embedding dependencies not found, install with `poetry install --extras llms-openai-like`"
+                    ) from e
+                openai_settings = settings.openai
+                self.embedding_model = OpenAILikeEmbedding(
+                    api_base=openai_settings.api_base,
+                    api_key=openai_settings.api_key,
+                    model=openai_settings.embedding_model,
+                )
+
+            case "ollama":
+                try:
+                    from llama_index.embeddings.ollama import OllamaEmbedding  # type: ignore
+                except ImportError as e:
+                    raise ImportError(
+                        "Ollama embedding dependencies not found, install with `poetry install --extras llms-ollama`"
                     ) from e
 
                 ollama_settings = settings.ollama
-
-                # Calculate embedding model. If not provided tag, it will be use latest
                 model_name = (
                     ollama_settings.embedding_model + ":latest"
                     if ":" not in ollama_settings.embedding_model
                     else ollama_settings.embedding_model
                 )
-
                 self.embedding_model = OllamaEmbedding(
-                    model_name=model_name,
-                    base_url=ollama_settings.embedding_api_base,
+                    model=model_name,
+                    base_url=ollama_settings.api_base,
                 )
-
-                if ollama_settings.autopull_models:
-                    if ollama_settings.autopull_models:
-                        from private_gpt.utils.ollama import (
-                            check_connection,
-                            pull_model,
-                        )
-
-                        # TODO: Reuse llama-index client when llama-index is updated
-                        client = Client(
-                            host=ollama_settings.embedding_api_base,
-                            timeout=ollama_settings.request_timeout,
-                        )
-
-                        if not check_connection(client):
-                            raise ValueError(
-                                f"Failed to connect to Ollama, "
-                                f"check if Ollama server is running on {ollama_settings.api_base}"
-                            )
-                        pull_model(client, model_name)
 
             case "azopenai":
                 try:
-                    from llama_index.embeddings.azure_openai import (  # type: ignore
-                        AzureOpenAIEmbedding,
-                    )
+                    from llama_index.embeddings.azure_openai import AzureOpenAIEmbedding  # type: ignore
                 except ImportError as e:
                     raise ImportError(
-                        "Azure OpenAI dependencies not found, install with `poetry install --extras embeddings-azopenai`"
+                        "Azure OpenAI embedding dependencies not found, install with `poetry install --extras llms-azopenai`"
                     ) from e
 
                 azopenai_settings = settings.azopenai
@@ -130,38 +102,24 @@ class EmbeddingComponent:
                     azure_endpoint=azopenai_settings.azure_endpoint,
                     api_version=azopenai_settings.api_version,
                 )
+
             case "gemini":
                 try:
-                    from llama_index.embeddings.gemini import (  # type: ignore
-                        GeminiEmbedding,
-                    )
+                    from llama_index.embeddings.gemini import GeminiEmbedding  # type: ignore
                 except ImportError as e:
                     raise ImportError(
-                        "Gemini dependencies not found, install with `poetry install --extras embeddings-gemini`"
+                        "Google Gemini embedding dependencies not found, install with `poetry install --extras llms-gemini`"
                     ) from e
-
+                gemini_settings = settings.gemini
                 self.embedding_model = GeminiEmbedding(
-                    api_key=settings.gemini.api_key,
-                    model_name=settings.gemini.embedding_model,
+                    model_name=gemini_settings.embedding_model, api_key=gemini_settings.api_key
                 )
-            case "mistralai":
-                try:
-                    from llama_index.embeddings.mistralai import (  # type: ignore
-                        MistralAIEmbedding,
-                    )
-                except ImportError as e:
-                    raise ImportError(
-                        "Mistral dependencies not found, install with `poetry install --extras embeddings-mistral`"
-                    ) from e
 
-                api_key = settings.openai.api_key
-                model = settings.openai.embedding_model
+            case "venice":
+                self.embedding_model = VeniceEmbedding()  # Uses env for embedding model
+            case "morpheus":
+                self.embedding_model = MorpheusEmbedding()  # Uses env for embedding model
 
-                self.embedding_model = MistralAIEmbedding(
-                    api_key=api_key,
-                    model=model,
-                )
             case "mock":
-                # Not a random number, is the dimensionality used by
-                # the default embedding model
-                self.embedding_model = MockEmbedding(384)
+                from llama_index.core.embeddings import MockEmbedding
+                self.embedding_model = MockEmbedding()
